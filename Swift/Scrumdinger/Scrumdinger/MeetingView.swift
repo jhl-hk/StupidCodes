75ac50249a962aec0d8456f529c257b8ba318381
jhl-hk <valor@jianyuelab.org>
2025-10-31 21:53:44 +0900

    Inig

--- Swift/Scrumdinger/Scrumdinger/MeetingView.swift
+++ Swift/Scrumdinger/Scrumdinger/MeetingView.swift
@@ -10,12 +10,26 @@ import SwiftUI
 struct MeetingView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Time Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Time Remaining")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+            }
         }
-        .padding()
     }
 }
 
