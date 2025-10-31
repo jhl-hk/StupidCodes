dbd25b321cfb53fad3e23ae0211cc1a573fb037f
jhl-hk <valor@jianyuelab.org>
2025-10-31 21:53:38 +0900

    Init Swift Scrumdinger

---
+++ Swift/Scrumdinger/Scrumdinger/MeetingView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Jianyue Hugo Liang on 2025/10/31.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MeetingView()
+}
---
+++ Swift/Scrumdinger/Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Jianyue Hugo Liang on 2025/10/31.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
